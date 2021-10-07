74ec2b41273524e6d1f30d72d7eeed65a6c21fe6
maxyypoo <82862519+maxyypoo@users.noreply.github.com>
2021-10-06 17:55:08 -0700

    upd 2, pseudo code

--- ai-flower.py
+++ ai-flower.py
@@ -6,11 +6,38 @@ import os
 import json
 from flask import Flask, render_template, requests, jsonify
 
-def sql_con():
-  try:
-    con = sqlite3.connect('mydb.db')
-    return con
-  except Error:
-    print (Error)
-    
+
+
+
+
 con = sql_con()
+
+@app.route('/')
+def root():
+  return render_template('index.html')
+
+@app.route('/button_click', methods = ['POST'])
+def clicked():
+  #some information from front end, like qr_code id = ?, location_id = ?, js_token_string = ?, flower_id = ? in the form of list of dictionaries: ([key, pair], [key, pair] ...)
+  frontEndInfo = requests.json['infos']
+  #or information is passed individually as single strings: "location_id"
+  frontEndInfo1 = requests.json['info']
+  frontEndInfo2 = requests.json['info']
+  #and so on...
+  #we make a SELECT call to our db, using the sql class I wrote, or we make custom SELECT calls here to figure out the growth level of the flower or if the flower even exists for the given location yet
+  myCursor = con.cursor()
+  myCursor.execute('SELECT information FROM one_of_our_tables WHERE some_identifying_info = frontEndInfo')
+  receivedInfo = myCursor.fetchall
+  
+@app.route('/display', methods = ['POST'])
+def displayFlower():
+  #here we calculate the flower that will be displayed based on previously acquired info, and send the specifications to front end
+  
+
+@atexit.register
+def shutdown():
+    if client:
+        client.disconnect()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port, debug=True)
--- sql.py
+++ sql.py
@@ -17,11 +17,27 @@ class sql:
   
   def insertIntoFlower(con, entities):
     myCursor = con.cursor()
-    myCursor.execute('INSERT INTO flower (growth_lvl, date_last_attended, _unique_clicks) VALUES (?, ?, ?)' , entities)
+    myCursor.execute('INSERT INTO flower (growth_lvl, date_last_attended, unique_clicks) VALUES (?, ?, ?)' , entities)
     myCursor.commit()
     
     
   def insertIntoQr(con, entities):
     myCursor = con.cursor()
     myCursor.execute('INSERT INTO js (js_token_string, is_active) VALUES (?, ?)' , entities)
+    myCursor.commit()
+    
+  def updateFlower(con, entities):
+    myCursor = con.cursor()
+    myCursor.execute('UPDATE flower SET growth_lvl = growth_lvl + 1, date_last_attended = ?, unique_clicks = ? WHERE flower_id = (SELECT flower_id from js WHERE js_token_string = ?)' , entities)
+    myCursor.commit()
+    
+    
+  def updateJs(con, entities):
+    myCursor = con.cursor()
+    myCursor.execute('UPDATE js SET is_active = ? WHERE js_token_string = ?' , entities)
+    myCursor.commit()
+    
+  def updateLoc(con, entities):
+    myCursor = con.cursor()
+    myCursor.execute('UPDATE location SET total_visits = ?, total_unique_visits = ? WHERE location_id = ?' , entities)
     myCursor.commit()
